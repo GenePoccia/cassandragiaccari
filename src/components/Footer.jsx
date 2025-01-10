bdd82a041e39209297f2838971672729d823d074
Eugenio <pocciaeugenio@gmail.com>
2025-01-10 18:20:56 -0500

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Footer = () => {
-	return <div>footer</div>;
+	return <div>footer test</div>;
 };
 
 export default Footer;
